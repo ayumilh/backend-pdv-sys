7f8eadf42b860385b2e9b817df174719f815ca91
ayumilh <ayumi.cod06@gmail.com>
2025-08-06 19:08:07 -0300

    remove configuração de porta e inicialização do servidor no arquivo app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -45,11 +45,4 @@ app.use('/api/userauth', authRoutes, userRoutes);
 
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 3001;
-
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
 export default app;
