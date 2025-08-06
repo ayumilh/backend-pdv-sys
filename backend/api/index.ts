6b82d591eb5d9cf2e1ada08b6b9245e352de6d5e
ayumilh <ayumi.cod06@gmail.com>
2025-08-06 18:56:55 -0300

    adiciona configuração inicial do servidor Express e rotas da API

---
+++ backend/api/index.ts
@@ -0,0 +1,5 @@
+// api/index.ts
+import app from '../app.js';   // seu Express app
+
+// Apenas isso: exporte o próprio app
+export default app;
---
+++ backend/app.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./src/modules/auth/auth.routes";
+import usuariosRoutes from "./src/modules/usuarios/usuarios.routes";
+import userRoutes from "./src/modules/user/user.routes";
+import productsRoutes from "./src/modules/product/product.routes";
+import categorysRoutes from "./src/modules/category/category.routes";
+import stockRoutes from "./src/modules/stock/stock.routes";
+import cashRoutes from "./src/modules/cash/cash.routes";
+
+import { errorMiddleware } from "./src/shared/middleware/errorMiddleware";
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "https://interface-pdv-sys.vercel.app", 
+      "http://localhost:3000"
+    ],
+    credentials: true,
+  })
+);
+app.options("*", cors());
+
+// Rota 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bem-vindo à página principal");
+});
+
+app.use("/api/usuarios", usuariosRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/categories", categorysRoutes);
+app.use("/api/stock", stockRoutes);
+app.use("/api/cash", cashRoutes);
+app.use('/api/userauth', authRoutes, userRoutes);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export default app;
